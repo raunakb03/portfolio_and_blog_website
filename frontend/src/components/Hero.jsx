b5e2b32d6babd170f017451430ef073c6592b9b2
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2023-06-30 16:35:05 +0530

    making mobile screen responsive

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -42,16 +42,16 @@ const Hero = () => {
         <div className="hello-animation w-fit text-center mx-auto text-[3.125rem]">
           👋
         </div>
-        <div className="text-[3.5rem] text-center text-[#111] tracking-[2px] font-[800]">
+        <div className=" text-[2.1rem] md:text-[3.5rem] text-center text-[#111] tracking-[1px] md:tracking-[2px] font-[800]">
           HEY, I{`'`}M RAUNAK BHALOTIA
         </div>
-        <div className="mt-[3rem] mx-auto font-[#333] text-[1.3rem] text-center w-[100%] leading-[1.6]">
+        <div className="mt-[3rem] mx-auto font-[#333] text-[0.8rem] md:text-[1.3rem] text-center w-[100%] leading-[1.6]">
           A Full Stack Web Developer building Full Stack Websites and Web
           Applications that leads to the success of the overall proudct
         </div>
         <div className="text-center mt-[5rem]">
           <Link
-            className="bg-[#facf0f] text-[#333] text-[1.3rem] uppercase tracking-[2px] inline-block font-[800] rounded-[5px] button-shadow transition-transform duration-[0.3s] py-[1rem] px-[6rem] hover:-translate-y-[5px] hover:scale-[1.1]"
+            className="bg-[#facf0f] text-[#333] text-[1.3rem] uppercase tracking-[2px] inline-block font-[800] rounded-[5px] button-shadow transition-transform duration-[0.3s] py-[0.5rem] md:py-[1rem] px-[3rem] md:px-[6rem] hover:-translate-y-[5px] hover:scale-[1.1]"
             to="/"
           >
             RESUME
@@ -60,7 +60,7 @@ const Hero = () => {
       </div>
       <a
         href="#about"
-        className="mouse absolute bottom-10 left-1/2 w-[25px] h-[40px] border-[2px] border-solid border-[#333] rounded-[60px]"
+        className="mouse absolute bottom-3 md:bottom-10 left-1/2 w-[25px] h-[40px] border-[2px] border-solid border-[#333] rounded-[60px]"
       ></a>
     </div>
   );
